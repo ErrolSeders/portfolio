671d1ec35207f66dcf90855d70c0496fb9c055cf
KauBoui <errolbucy@gmail.com>
2024-03-09 11:30:32 -0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+var router = express.Router();
+
+router.get("/", (er, res) => {
+  var options = { root: __dirname };
+  res.sendFile("public/index.html", options, (er) => {
+    if (er) {
+      res.writeHead(500);
+      res.end();
+    }
+  });
+});
+
+router.get("/projects", (er, res) => {
+  var options = { root: __dirname };
+  res.sendFile("public/projects.html", options, (er) => {
+    if (er) {
+      res.writeHead(500);
+      res.end();
+    }
+  });
+});
+
+router.get("/gallery", (er, res) => {
+  var options = { root: __dirname };
+  res.sendFile("public/gallery.html", options, (er) => {
+    if (er) {
+      res.writeHead(500);
+      res.end();
+    }
+  });
+});
+
+router.get("/contact", (er, res) => {
+  var options = { root: __dirname };
+  res.sendFile("public/contact.html", options, (er) => {
+    if (er) {
+      res.writeHead(500);
+      res.end();
+    }
+  });
+});
+
+router.get("/fractalanimation", (er, res) => {
+  var options = { root: __dirname };
+  res.sendFile("public/fractalanimation.html", options, (er) => {
+    if (er) {
+      res.writeHead(500);
+      res.end();
+    }
+  });
+});
+
+app.use("/", router);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
